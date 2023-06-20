fe4c9a30241bd83599068d7cd282be96f42101c8
熊志刚 <xiongzhigang@pwrd.com>
2023-06-20 19:43:50 +0800

    fix: add ouo

--- components/Ouo.js
+++ components/Ouo.js
@@ -1,22 +1,22 @@
-import BLOG from "@/blog.config";
+import BLOG from '@/blog.config'
 
 /**
  * 添加ouo
  * @returns
  */
 const Ouo = () => {
-  return (<>
-    {BLOG.OUO && BLOG.OUO_TOKEN && BLOG.OUO_DOMAINS && (<>
-        <script type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: `
+    return (<>
+        {BLOG.OUO && BLOG.OUO_TOKEN && BLOG.OUO_DOMAINS && (<>
+            <script type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
                      var ouo_token = '${BLOG.OUO_TOKEN}';
                      var domains = [${BLOG.OUO_DOMAINS}]; 
                    `
-                }}
-        />
-        <script src="//cdn.ouo.io/js/full-page-script.js" />
-    </>)}
-</>)
+                    }}
+            />
+            <script src="//cdn.ouo.io/js/full-page-script.js" />
+        </>)}
+    </>)
 }
 export default Ouo
