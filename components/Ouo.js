5c1205c87245a082c3f1e6ff7f37012aa420895b
熊志刚 <xiongzhigang@pwrd.com>
2023-06-20 19:47:45 +0800

    fix: add ouo

--- components/Ouo.js
+++ components/Ouo.js
@@ -5,18 +5,19 @@ import BLOG from '@/blog.config'
  * @returns
  */
 const Ouo = () => {
-    return (<>
-        {BLOG.OUO && BLOG.OUO_TOKEN && BLOG.OUO_DOMAINS && (<>
-            <script type="text/javascript"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                     var ouo_token = '${BLOG.OUO_TOKEN}';
-                     var domains = [${BLOG.OUO_DOMAINS}]; 
-                   `
-                    }}
-            />
-            <script src="//cdn.ouo.io/js/full-page-script.js" />
-        </>)}
-    </>)
+  return (<>
+    {BLOG.OUO && BLOG.OUO_TOKEN && BLOG.OUO_DOMAINS && (<>
+      <script type="text/javascript"
+              dangerouslySetInnerHTML={{
+                  __html: `
+               var ouo_token = '${BLOG.OUO_TOKEN}';
+               var domains = [${BLOG.OUO_DOMAINS}]; 
+             `
+              }}
+      />
+      <script src="//cdn.ouo.io/js/full-page-script.js" />
+    </>)}
+  </>)
 }
+
 export default Ouo
